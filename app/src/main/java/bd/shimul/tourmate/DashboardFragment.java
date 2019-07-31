867217cc18edd10600f521ebba2c97d3ebe7802e
almamurshimul <abdullah15-6566@diu.edu.bd>
2019-07-31 20:48:18 +0600

    Home activity changed and dashboard design complete.

---
+++ app/src/main/java/bd/shimul/tourmate/DashboardFragment.java
@@ -0,0 +1,53 @@
+package bd.shimul.tourmate;
+
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+
+public class DashboardFragment extends Fragment {
+
+    public DashboardFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        init(view);
+
+        return view;
+    }
+
+    private void init(View view) {
+
+
+    }
+
+}
--- app/src/main/java/bd/shimul/tourmate/HomeActivity.java
+++ app/src/main/java/bd/shimul/tourmate/HomeActivity.java
@@ -1,55 +1,60 @@
 package bd.shimul.tourmate;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.service.carrier.CarrierMessagingService;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
-    FirebaseAuth firebaseAuth;
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_dash:
+                    setFragment(new DashboardFragment());
+                    return true;
+                case R.id.navigation_tour:
+                   setFragment(new ToursFragment());
+                    return true;
+                case R.id.navigation_nearby:
+                    setFragment(new NearbyFragment());
+                    return true;
+
+                case R.id.navigation_weather:
+                    setFragment(new WeatherFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        init();
-
-
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        setFragment(new DashboardFragment());
     }
 
-    private void init() {
-        firebaseAuth = FirebaseAuth.getInstance();
+    private void setFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.home_frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = new MenuInflater(this);
-        menuInflater.inflate(R.menu.logout_menu, menu);
-
-        return true;
-
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.logout){
-            firebaseAuth.signOut();
-            finish();
-            Intent intent = new Intent(HomeActivity.this, MainActivity.class);
-            startActivity(intent);
-
-
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
